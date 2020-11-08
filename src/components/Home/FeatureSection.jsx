e84bd36af2714572da50b385d14b34f530c44bda
Yew Kang Wei <yewyew6933@gmail.com>
2020-11-09 00:39:26 +0800

    feature setup

--- src/components/Home/FeatureSection.jsx
+++ src/components/Home/FeatureSection.jsx
@@ -1,58 +1,82 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function FeatureSection() {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: { relativeDirectory: { eq: "HomeImages/FeatureImages" } }
+        sort: { fields: name }
+      ) {
+        edges {
+          node {
+            image: childImageSharp {
+              fixed(quality: 100, width: 70) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const { edges } = data.allFile
+
+  const featureCols = [
+    {
+      title: "safe",
+      description: "designed to avoid hurting pets",
+      imgAlt: "safe cat toy",
+    },
+    {
+      title: "non-chemical",
+      description: "made with chewable materials",
+      imgAlt: "non-toxic cat toy",
+    },
+    {
+      title: "affordable",
+      description: "aligned with the best market prices",
+      imgAlt: "cheap cat toy",
+    },
+    {
+      title: "fast Delivery",
+      description: "reach your place on time",
+      imgAlt: "reliable cat toy delivery",
+    },
+  ]
+
   return (
-    <section className="feature py-5 my-5">
-      <div className="feature__headline">
-        <div className="feature__headline__heading heading text-center text-capitalize h1 mb-3">
+    <section className="Feature py-5 my-5">
+      <div className="Feature__headline">
+        <div className="Feature__headline__heading heading text-center text-capitalize h1 mb-3">
           We provide high quality toy
         </div>
-        <div className="feature__headline__subheading text-center">
+        <div className="Feature__headline__subheading text-center">
           Shipping good toys and ensuring your cat's health and happiness are
           what we love and so passionate about.
         </div>
       </div>
 
-      <div className="feature__row row mt-5">
-        <div className="feature__row__item col-md-3 all-center-column">
-          {/* <img src="./images/feature_safe.png" alt="safe cat toy" className="feature__row__item__image"> */}
-          <div className="feature__row__item__title heading mt-4 mb-2">
-            Safe
+      <div className="Feature__row row mt-5">
+        {featureCols.map((featureCol, index) => (
+          <div
+            className="Feature__row__item col-md-3 all-center-column"
+            key={index}
+          >
+            <img
+              src={edges[index].node.image.fixed.src}
+              alt={featureCol.imgAlt}
+              className="Feature__row__item__image"
+            />
+            <div className="Feature__row__item__title heading mt-4 mb-2 text-capitalize">
+              {featureCol.title}
+            </div>
+            <div className="Feature__row__item__subtitle text-center text-capitalize">
+              {featureCol.description}
+            </div>
           </div>
-          <div className="feature__row__item__subtitle text-center">
-            Designed to avoid hurting pets
-          </div>
-        </div>
-
-        <div className="feature__row__item col-md-3 all-center-column">
-          {/* <img src="./images/feature_xchemical.png" alt=""> */}
-          <div className="feature__row__item__title heading mt-4 mb-2">
-            Non-chemical
-          </div>
-          <div className="feature__row__item__subtitle text-center">
-            Made with chewable materials
-          </div>
-        </div>
-
-        <div className="feature__row__item col-md-3 all-center-column">
-          {/* <img src="./images/feature_affordable.png" alt=""> */}
-          <div className="feature__row__item__title heading mt-4 mb-2">
-            Affordable
-          </div>
-          <div className="feature__row__item__subtitle text-center">
-            Aligned with the best market prices
-          </div>
-        </div>
-
-        <div className="feature__row__item col-md-3 all-center-column">
-          {/* <img src="./images/feature_fast.png" alt=""> */}
-          <div className="feature__row__item__title heading mt-4 mb-2">
-            Fast Delivery
-          </div>
-          <div className="feature__row__item__subtitle text-center">
-            Reach your place on time
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
