8379916ddf0abb5ff9a3f70c06cd44a47a207178
Yew Kang Wei <yewyew6933@gmail.com>
2020-11-15 12:29:51 +0800

    render blogs in blog section

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/Home/BlogSection.jsx
+++ src/components/Home/BlogSection.jsx
@@ -1,85 +1,72 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 export default function BlogSection() {
+  const data = useStaticQuery(graphql`
+    {
+      blogs: allContentfulBlogs(sort: { fields: date, order: DESC }) {
+        edges {
+          node {
+            id
+            title
+            slug
+            date(formatString: "DD MMMM YYYY")
+            image {
+              fluid {
+                src
+              }
+            }
+            article: previewedArticle {
+              markdown: childMarkdownRemark {
+                excerpt(pruneLength: 200)
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
-    <section className="blog py-5 my-5">
-      <div className="blog__headline">
-        <div className="blog__headline__heading Heading text-center text-capitalize h1 mb-3">
+    <section className="Blog py-5 my-5">
+      <div className="Blog__headline">
+        <div className="Blog__headline__heading Heading text-center text-capitalize h1 mb-3">
           from our blog
         </div>
-        <div className="blog__headline__subheading text-center">
+        <div className="Blog__headline__subheading text-center">
           Here are some extra tips to teach you how to build a healthy
           relationship with your cats.
         </div>
       </div>
 
       <div className="container">
-        <div className="blog__row row mt-5">
-          <div className="blog__row__item col-lg-4 all-center">
-            <div className="card">
-              {/* <img className="card-img-top card__image" src="./images/feature_fast.png" alt="How to Get Your Cat to Play By Itself"> */}
-              <div className="blog__card card-body p-4">
-                <div className="blog__row__item__title mb-3">
-                  How to Get Your Cat to Play By Itself
+        <div className="Blog__row row mt-5">
+          {data.blogs.edges.map(blog => (
+            <div className="Blog__row__item col-lg-4 all-center">
+              <div className="card">
+                <img
+                  className="card-img-top card__image"
+                  src={blog.node.image.fluid.src}
+                  alt="How to Get Your Cat to Play By Itself"
+                />
+                <div className="Blog__card card-body p-4">
+                  <div className="Blog__row__item__title mb-3">
+                    {blog.node.title}
+                  </div>
+                  <div className="Blog__row__item__subtitle mb-3">
+                    {blog.node.article.markdown.excerpt}
+                  </div>
+                  <Link
+                    to={blog.node.slug}
+                    className="btn btn-outline-secondary btn-md"
+                  >
+                    Read more
+                  </Link>
                 </div>
-                <div className="blog__row__item__subtitle mb-3">something</div>
-                <Link to="/" className="btn btn-outline-secondary btn-md">
-                  Read more
-                </Link>
               </div>
             </div>
-          </div>
-
-          <div className="blog__row__item col-lg-4 all-center">
-            <div className="card">
-              {/* <img className="card-img-top card__image" src="./images/feature_fast.png" alt="How to Get Your Cat to Play By Itself"> */}
-              <div className="blog__card card-body p-4">
-                <div className="blog__row__item__title mb-3">
-                  How to Get Your Cat to Play By Itself
-                </div>
-                <div className="blog__row__item__subtitle mb-3">something</div>
-                <Link to="/" className="btn btn-outline-secondary btn-md">
-                  Read more
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="blog__row__item col-lg-4 all-center">
-            <div className="card">
-              {/* <img className="card-img-top card__image" src="./images/feature_fast.png" alt="How to Get Your Cat to Play By Itself"> */}
-              <div className="blog__card card-body p-4">
-                <div className="blog__row__item__title mb-3">
-                  How to Get Your Cat to Play By Itself
-                </div>
-                <div className="blog__row__item__subtitle mb-3">something</div>
-                <Link to="/" className="btn btn-outline-secondary btn-md">
-                  Read more
-                </Link>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
   )
 }
-
-// {
-//   coffeeItem: allContentfulCoffeeItem {
-//     edges {
-//       node {
-//         id
-//         title
-//         category
-//         image {
-//           description
-//           fixed(width: 50, height: 50) {
-//             src
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
