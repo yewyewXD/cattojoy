06dcc867a40bd8b49865aade26b64c223578dc1e
Yew Kang Wei <yewyew6933@gmail.com>
2020-12-27 08:01:19 +0800

    add CTA links and responsive hero + CTA

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,21 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function CallToAction() {
   return (
-    <section className="CTA py-5">
+    <section className="CallToAction | py-5">
       <div className="container all-center">
-        <div className="CTA__Caption col-md-6 text-center">
-          <div className="CTA__Caption__Title heading text-capitalize mb-3">
+        <div className="col-md-6 text-center">
+          <h3 className="CallToAction__Title | heading text-capitalize mb-3">
             let's build a healthy relationship with your cats
-          </div>
-          <div className="CTA__Caption__Description mb-3">
+          </h3>
+          <div className="mb-3">
             We know how important your pets are to you. We've created the most
             innovative toys to provide you with everything you need to keep your
             furry friends happy.
           </div>
-          <button className="btn btn-secondary btn-lg text-capitalize">
+          <Link to="/shop" className="btn btn-secondary btn-lg text-capitalize">
             join now
-          </button>
+          </Link>
         </div>
       </div>
     </section>
--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import BackgroundImage from "gatsby-background-image"
 
@@ -17,14 +17,14 @@ export default function HeroSection() {
   `)
 
   return (
-    <div className="HeroSection">
+    <section className="HeroSection">
       <BackgroundImage
         Tag="section"
         className="BgImage"
         fluid={data.images.childImageSharp.fluid}
       >
         <div className="BgOverlay">
-          <div className="CaptionContainer all-center">
+          <div className="CaptionContainer | all-center">
             {/* caption */}
             <div className="Caption | text-center text-white">
               <h1 className="Caption__Title | heading mb-2">
@@ -33,13 +33,13 @@ export default function HeroSection() {
               <h2 className="Caption__Subtitle">
                 Bring the best joy to your kitty with our innovative cat toys
               </h2>
-              <button className="btn btn-primary btn-lg mt-4">
+              <Link to="/shop" className="btn btn-primary btn-lg mt-4">
                 Discover now
-              </button>
+              </Link>
             </div>
           </div>
         </div>
       </BackgroundImage>
-    </div>
+    </section>
   )
 }
