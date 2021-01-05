d64a6956cfb1470c43bf3c6a01dfc417bb9ef077
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-05 23:37:37 +0800

    add products section

---
+++ src/components/ShopComponents/ProductsSection.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ProductsSection = () => {
+  return (
+    <section className="ProductsSection | row mb-3">
+      <div className="col-lg-4 mb-5" style={{ height: "400px" }}>
+        <div className="h-100 w-100 bg-dark"></div>
+      </div>
+    </section>
+  )
+}
+
+export default ProductsSection
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -3,16 +3,31 @@ import React from "react"
 import Layout from "../components/Layout"
 import PageBanner from "../components/ReusableComponents/PageBanner"
 import SEO from "../components/seo"
+import ProductsSection from "../components/ShopComponents/ProductsSection"
 
 const ShopPage = () => {
   return (
     <Layout currentPage="shop">
-      <PageBanner
-        description="Shopping Shopping Shopping Shopping Shopping Shopping Shopping Shopping Shopping"
-        title="Shopping Page"
-      />
-      <SEO title="Catto Joy Shop" />
-      <div className="ShopPage">hi</div>
+      <SEO title="Shop" />
+
+      <main className="ShopPage">
+        {/* banner */}
+        <PageBanner
+          description="Here you can find all the cat toys we are selling."
+          title="Products"
+        />
+
+        {/* content */}
+        <div className="container mt-5">
+          {/* view switcher */}
+          <div className="all-center justify-content-between mb-4">
+            <div>view switcher</div>
+            <div>sorting </div>
+          </div>
+
+          <ProductsSection />
+        </div>
+      </main>
     </Layout>
   )
 }
