3f5408599b3bf32b16a88d15a153b3f8bb1692f0
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-06 09:36:20 +0800

    render produtcs from cms

--- src/components/ShopComponents/ProductsSection.jsx
+++ src/components/ShopComponents/ProductsSection.jsx
@@ -1,11 +1,66 @@
 import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 const ProductsSection = () => {
+  const data = useStaticQuery(graphql`
+    {
+      products: allContentfulProducts {
+        edges {
+          node {
+            id
+            title
+            price
+            description {
+              markdown: childMarkdownRemark {
+                excerpt
+              }
+            }
+            previewImage {
+              fluid(quality: 100) {
+                src
+              }
+            }
+            slideImages {
+              fluid(quality: 100) {
+                src
+              }
+              fixed(quality: 100, width: 200) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <section className="ProductsSection | row mb-3">
-      <div className="col-lg-4 mb-5" style={{ height: "400px" }}>
-        <div className="h-100 w-100 bg-dark"></div>
-      </div>
+      {data.products.edges.map(({ node }) => (
+        <div
+          className="col-lg-4 mb-5"
+          style={{ height: "400px" }}
+          key={node.id}
+        >
+          {/* image */}
+          <div
+            className="h-75 w-100 bg-dark"
+            style={{
+              backgroundImage: `url(${node.previewImage.fluid.src})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+
+          {/* description */}
+          <div className="text-center all-center justify-content-between w-100 mt-2">
+            <span>{node.title}</span>
+
+            <span>RM{node.price}</span>
+          </div>
+        </div>
+      ))}
     </section>
   )
 }
