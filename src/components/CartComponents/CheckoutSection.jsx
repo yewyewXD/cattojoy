541384a679f3f5cdbc332acb0e1bc1538b15a71a
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-12 17:07:14 +0800

    get accurate total

--- src/components/CartComponents/CheckoutSection.jsx
+++ src/components/CartComponents/CheckoutSection.jsx
@@ -1,7 +1,33 @@
-import React from "react"
+import React, { useContext } from "react"
+import { CartContext } from "../../context/CartContext/CartState"
+import { handleRoundPrice } from "../../utils/price"
 
 const CheckoutSection = () => {
-  return <section className="CheckoutSection | mt-3 bg-light">checkout</section>
+  const { products } = useContext(CartContext)
+
+  function getTotalPrice() {
+    const totalPrice = products
+      .map(product => +product.count * +product.price)
+      .reduce((price, nextPrice) => price + nextPrice)
+
+    return totalPrice
+  }
+
+  return (
+    <section className="CheckoutSection | mt-3 bg-light p-3 all-center-column align-items-end">
+      <div>
+        <div>
+          <b>Subtotal: </b>
+          <span>RM{getTotalPrice()}</span>
+        </div>
+
+        <div>
+          <b>Total Payment: </b>
+          <span>RM{handleRoundPrice(getTotalPrice())}</span>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default CheckoutSection
