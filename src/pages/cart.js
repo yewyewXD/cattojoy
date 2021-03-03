03ac762d1f2f0f066f72dec8278aaa010c9dfafc
Yew Kang Wei <yewyew6933@gmail.com>
2021-03-03 13:09:12 +0800

    carrt is empty

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PageBanner from "../components/ReusableComponents/PageBanner"
 import ShoppingCart from "../components/CartComponents/ShoppingCart"
 import CheckoutSection from "../components/CartComponents/CheckoutSection"
+import { CartContext } from "../context/CartContext/CartState"
+import { graphql, useStaticQuery } from "gatsby"
 
 const CartPage = () => {
+  const { products } = useContext(CartContext)
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "CartImages/catInBasket.png" }) {
+        image: childImageSharp {
+          fixed(quality: 70, width: 200) {
+            src
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout currentPage="cart">
       <SEO title="My Cart" />
@@ -28,6 +43,13 @@ const CartPage = () => {
               <div className="col-2 d-lg-block d-none">Total</div>
             </div>
           </div>
+
+          {products.length === 0 && (
+            <div className="all-center-column py-3">
+              <img src={data.file.image.fixed.src} alt="Cart is empty" />
+              <div>Cart is empty.</div>
+            </div>
+          )}
 
           {/* shopping cart */}
           <ShoppingCart />
