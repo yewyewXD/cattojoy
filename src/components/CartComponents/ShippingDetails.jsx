9979001a532b8c57aa4a2b391f5fb70dec7b2a18
Yew Kang Wei <yewyew6933@gmail.com>
2021-03-08 08:28:13 +0800

    clean up

--- src/components/CartComponents/ShippingDetails.jsx
+++ src/components/CartComponents/ShippingDetails.jsx
@@ -1,49 +1,57 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+import useDidMountEffect from "../../utils/useDidMountEffect"
 
-const ShippingDetails = ({
-  setName,
-  setEmail,
-  setAddressOne,
-  setAddressTwo,
-  setCity,
-  setState,
-  setPostal,
-}) => {
+const ShippingDetails = ({ setShippingDetails, isValidating }) => {
+  const detailUpperRef = useRef()
+  const detailLowerRef = useRef()
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [addressOne, setAddressOne] = useState("")
+  const [addressTwo, setAddressTwo] = useState("")
+  const [city, setCity] = useState("")
+  const [state, setState] = useState("")
+  const [postal, setPostal] = useState("")
+
+  useDidMountEffect(() => {}, [isValidating])
+
   return (
     <div className="ShippingDetails | bg-white rounded shadow-sm">
       <h5 className="ShippingDetails__Title | heading">SHIPPING DETAILS</h5>
 
+      <div ref={detailUpperRef}></div>
       <div className="InputContainer">
-        <input id="cardholderName" type="text" required />
+        <input id="" type="text" value={name} required />
         <div className="InputLine"></div>
-        <label htmlFor="cardholderName">Full name</label>
+        <label htmlFor="">Full name</label>
         <div className="InputContainer__Error">
           <small>error</small>
         </div>
       </div>
 
       <div className="InputContainer">
-        <input id="cardNumber" type="email" required />
+        <input id="" type="email" value={email} required />
         <div className="InputLine"></div>
-        <label htmlFor="cardNumber">Email</label>
+        <label htmlFor="">Email</label>
         <div className="InputContainer__Error">
           <small>error</small>
         </div>
       </div>
 
       <div className="InputContainer">
-        <input id="cardNumber" type="text" required />
+        <input id="" type="text" value={addressOne} required />
         <div className="InputLine"></div>
-        <label htmlFor="cardNumber">Address Line 1</label>
+        <label htmlFor="">Address Line 1</label>
         <div className="InputContainer__Error">
           <small>error</small>
         </div>
       </div>
 
+      <div ref={detailLowerRef}></div>
       <div className="InputContainer">
-        <input id="cardNumber" type="text" required />
+        <input id="" type="text" value={addressTwo} required />
         <div className="InputLine"></div>
-        <label htmlFor="cardNumber">Address Line 2</label>
+        <label htmlFor="">Address Line 2</label>
         <div className="InputContainer__Error">
           <small>error</small>
         </div>
@@ -51,27 +59,27 @@ const ShippingDetails = ({
 
       <div className="d-flex justify-content-md-between flex-md-row flex-column">
         <div className="InputContainerTight">
-          <input id="cardNumber" type="text" required />
+          <input id="" type="text" value={city} required />
           <div className="InputLine"></div>
-          <label htmlFor="cardNumber">City</label>
+          <label htmlFor="">City</label>
           <div className="InputContainer__Error">
             <small>error</small>
           </div>
         </div>
 
         <div className="InputContainerTight">
-          <input id="cardNumber" type="text" required />
+          <input id="" type="text" value={state} required />
           <div className="InputLine"></div>
-          <label htmlFor="cardNumber">State</label>
+          <label htmlFor="">State</label>
           <div className="InputContainer__Error">
             <small>error</small>
           </div>
         </div>
 
         <div className="InputContainerTight">
-          <input id="cardNumber" type="number" required />
+          <input id="" type="number" value={postal} required />
           <div className="InputLine"></div>
-          <label htmlFor="cardNumber">Zip code</label>
+          <label htmlFor="">Zip code</label>
           <div className="InputContainer__Error">
             <small>error</small>
           </div>
--- src/utils/fieldChange.js
+++ src/utils/fieldChange.js
@@ -1,19 +1,17 @@
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export function handleTextChange(setField, element) {
+  const value = element.target.value
   setField({
-    content: element.target.value,
-    isValid: element.target.value
-      ? element.target.value.toString().trim().length > 0
-      : false,
+    content: value,
+    isValid: value ? value.toString().trim().length > 0 : false,
   })
 }
 
 export function handleEmailChange(setField, element) {
+  const value = element.target.value
   setField({
-    content: element.target.value,
-    isValid: element.target.value
-      ? emailRegex.test(String(element.target.value).toLowerCase())
-      : false,
+    content: value,
+    isValid: value ? emailRegex.test(String(value).toLowerCase()) : false,
   })
 }
