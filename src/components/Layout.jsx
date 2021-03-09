c678ea1047bd9cb0bdfa0ebc1967169ee8a645a2
Yew Kang Wei <yewyew6933@gmail.com>
2021-03-09 17:10:25 +0800

    use test key on production

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { Elements } from "@stripe/react-stripe-js"
 
 import Header from "./ReusableComponents/Header"
 
-const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_TEST)
 
 const Layout = ({ children, currentPage }) => {
   return (
