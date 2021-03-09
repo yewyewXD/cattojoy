91cdd5d7fee0613af1bd74b736e7540aca3fa9bf
Yew Kang Wei <yewyew6933@gmail.com>
2021-03-09 19:43:00 +0800

    setup success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,12 +2,40 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-const SuccessPage = () => (
-  <Layout>
-    <SEO title="Success Page" description="Welcome to Catto Joy" />
-    <main className="SuccessPage">success page</main>
-  </Layout>
-)
+const SuccessPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "404.png" }) {
+        image: childImageSharp {
+          fixed(quality: 60, width: 500) {
+            src
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout currentPage="404">
+      <SEO title="Success Page" />
+      <main className="SuccessPage | all-center-column w-100">
+        <h2 className="mb-4 text-center px-1">404 Page Not Found</h2>
+        <img
+          className="SuccessPage__Image | mb-sm-5 mb-4"
+          src={data.file.image.fixed.src}
+          alt="404 Page Not Found"
+        />
+        <h1 className="SuccessPage__Description | text-center px-1">
+          Meow, it seems like you're lost
+        </h1>
+        <Link className="btn btn-secondary btn-lg mt-4" to="/">
+          Back to Homepage
+        </Link>
+      </main>
+    </Layout>
+  )
+}
 
 export default SuccessPage
