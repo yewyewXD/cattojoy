4bdca6f84840ff66a90a59c2ebbb7bc56fc3f1f5
Yew Kang Wei <yewyew6933@gmail.com>
2021-03-10 16:52:30 +0800

    setup paypal

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-paypal`,
+      options: {
+        clientId: process.env.PAYPAL_CLIENT,
+        currency: `MYR`,
+        vault: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
